3c8a65b6a0ae0b2d69c865cde8728e6549d72ad7
DACHXY <Danny10132024@gmail.com>
2024-04-29 12:52:28 +0800

    build: 👷 change logger format

--- main.py
+++ main.py
@@ -155,4 +155,7 @@ if __name__ == "__main__":
     LOGGING_CONFIG["formatters"]["default"][
         "fmt"
     ] = "%(asctime)s %(levelprefix)s %(message)s"
+    LOGGING_CONFIG["formatters"]["access"][
+        "fmt"
+    ] = '%(asctime)s %(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s'
     uvicorn.run(app="main:app", host="0.0.0.0", port=8000)
