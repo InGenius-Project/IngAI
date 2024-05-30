4ee76677a08436602bad07f3a51f19a7f70da42a
DACHXY <Danny10132024@gmail.com>
2024-05-30 13:11:17 +0800

    perf: 👌 change to gpt-4o model

--- config/open_api_config.py
+++ config/open_api_config.py
@@ -1,7 +1,7 @@
 OPENAI_PROTOCAL = "http://"
 OPENAI_HOST = "api.openai.com"
 OPENAI_API = OPENAI_PROTOCAL + OPENAI_HOST + "/v1/models"
-OPENAI_MODEL = "gpt-4-turbo-preview"
+OPENAI_MODEL = "gpt-4o"
 
 CHAT_BOT_SYSTEM_LIMITATION_PROMPT = """是一個聊天機器人，並且只能回答實習有關的問題，其他一律回答我不知道你/妳在問甚麼\n"""
 ANALYZATION_USER_PREFIX_PROMPT = "以提供的事實資料(事實資料絕對為真)以及常識為基礎, 提供150字左右的徵才文章詐騙分析報告給我:\n"
